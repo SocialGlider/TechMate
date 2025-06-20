836b226a1c00de048e180a7c573f66c3281bd530
Palak <palakgupta182002@gmail.com>
2025-06-21 00:50:29 +0530

    Error Handling

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
-
+const globalErrorHandler = require('./controllers/errorController');
 const path= require('path');
+const AppError = require('./utils/appError');
 
 const app = express();
+
 
 app.use("/",express.static("uploads"));
 
@@ -31,4 +33,11 @@ app.use(express.json({limit: "10kb"}));
 
 app.use(mongoSanitize());
 
+app.all("*",(req,res,next)=>{
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`,404));
+});
+
+
+
+app.use(globalErrorHandler);
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+process.on("uncaughtException",(err)=>{
+    console.log("UNCAUGHT EXCEPTION! Shutting down");
+    console.log(err.name,err.message);
+    process.exit(1);
+});
 dotenv.config({path:"./config.env"});
 
 const app = require("./app");
@@ -12,4 +17,11 @@ const port = process.env.PORT || 3000;
 
 const server = app.listen(port, ()=>{
     console.log(`App Running on port ${port}`);  
+});
+process.on("unhandledRejection",(err)=>{
+    console.log("UNHANDLED REJECTION! Shutting down");
+    console.log(err.name,err.message);
+    server.close(()=>{
+      process.exit(1);
+    });
 });
