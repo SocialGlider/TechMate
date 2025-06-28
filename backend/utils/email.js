c4fbf6dd3027f5864ef0b8a89cffb6f03ce30930
mausumi25 <mausumirautaray2002@gmail.com>
2025-06-28 10:59:30 +0530

    email template

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,1 +1,23 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require('nodemailer');
+
+const sendEmail = async(options)=>{
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
+
+    const mailOptions ={
+        form: `"Pixora" Share Your Imagination in Every Color`,
+        to:options.email,
+        subject: options.subject,
+        html: options.html,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+};
+
+module.exports = sendEmail;
