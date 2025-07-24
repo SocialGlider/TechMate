c1a38f53169ae92d4621810b2f1e71483d174cc7
Palak-gupta18 <palakgupta182002@gmail.com>
2025-07-24 23:27:37 +0530

    reset-password

---
+++ frontend/app/auth/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PasswordReset from '@/components/Auth/PasswordReset'
+const ResetPasswordPage=() => {
+  return (
+   <PasswordReset/>
+  )
+}
+
+export default ResetPasswordPage
