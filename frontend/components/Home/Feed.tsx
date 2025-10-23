7dda1f4de55e9c5d6e8f879cb2826a0b694ef668
mausumi25 <mausumirautaray2002@gmail.com>
2025-10-23 23:45:32 +0530

    feed2

--- frontend/components/Home/Feed.tsx
+++ frontend/components/Home/Feed.tsx
@@ -6,6 +6,11 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { handleAuthRequest } from "../utils/apiRequest";
 import { setPost } from "@/store/postSlice";
+import { Loader } from "lucide-react";
+import Post from "../Profile/Post";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import DotButton from "../Helper/DotButton";
+import Image from "next/image";
 
 const Feed = () => {
   const dispatch = useDispatch();
@@ -28,9 +33,53 @@ const Feed = () => {
     getAllPost();
   },[dispatch]);
 
+  const handleLikeDislike = async (id: string) => {};
+
+  const handleSaveUnsave = async (id: string)=> {};
+
+  const handleComment = async (id:string)=>{};
+
+  // handle Loading state
+  if(isLoading) {
+    return (
+      <div className="W-full h-screen flex items-center justify-center flex-col">
+        <Loader className="animate-spin"/>
+      </div>
+    );
+  }
+
+  if(posts.length<1){
+    return(
+      <div className="text-3xl m-8 text-center capitalize font-bold">
+        No Post To Show
+      </div>
+    );
+  }
+
   return (
-    <div>Feed</div>
+    <div className="mt-20 w-[70%] mx-auto">
+      {/* Main Post */}
+      {posts.map((Post)=>{
+        return <div key={Post._id} className="mt-8">
+          <div className="flex items-center justify-between">
+            {/* User info */}
+            <div className="flex items-center space-x-2">
+              <Avatar className="w-9 h-9">
+                <AvatarImage src={Post.user?.profilePicture} className="h-full w-full"/>
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h1>{Post.user?.username}</h1>
+            </div>
+              <DotButton />
+          </div>
+          {/* Image */}
+          <div className="mt-2">
+            <Image src={`${Post.image?.url}`} alt="Post" width={400} height={400} className="w-full"/>
+          </div>
+        </div>
+      })}
+    </div>
   )
-}
+};
 
 export default Feed
